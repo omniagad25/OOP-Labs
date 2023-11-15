b82b3515decbfafd293e0265678f514d5a24f2ab
omniagad25 <81337480+omniagad25@users.noreply.github.com>
2023-11-15 18:36:43 +0200

    main.cpp

--- Lab6 OOP/main.cpp
+++ Lab6 OOP/main.cpp
@@ -1,108 +1,175 @@
 #include <iostream>
-
+#include<graphics.h>
 using namespace std;
 
 class Point
 {
     int x,y;
-    public:
-        void set_x(int _x){
-            x=_x;
-        }
-        void set_y(int _y){
+public:
+    void set_x(int _x)
+    {
+        x=_x;
+    }
+    void set_y(int _y)
+    {
         y= _y;
-        }
-        void set_xy(int _x, int _y){
-            x=_x;
-            y=_y;
-        }
-        void paint(){
-            cout<<"("<<x<<","<<y<<")"<<endl;
-        }
-        Point(int _x,int _y){
-            x=_x;
-            y=_y;
-        }
-        Point(){
-            x=0;
-            y=0;
-            cout<<"constructor point"<<endl;
-        };
-        ~Point(){
-            cout<<"Destroy Point"<<endl;
-        }
-        class Shape{
-            protected: int color;
-            protected: Point p1;
-            protected: Point p2;
-            public:
-                void setColor(int _color){
-                    color=_color;
-                }
-                void setPoint1(int _x,int _y)
-                {
-                  p1.set_xy(_x,_y);
-                  }
-                void setPoint2(int _x,int _y)
-                 {
-                 p2.set_xy(_x,_y);
-                  }
-                Shape(){
-                    color=1;
-                    p1=(0,0);
-                    p2=(0,0);
-                    cout<<"Constructor shape"<<endl;
-                }
-        };
+    }
+    void set_xy(int _x, int _y)
+    {
+        x=_x;
+        y=_y;
+    }
+    int get_x(){
+        return x;
+    }
+    int get_y(){
+        return y;
+    }
+    void paint()
+    {
+        cout<<"("<<x<<","<<y<<")"<<endl;
+    }
+    Point(int _x,int _y)
+    {
+        x=_x;
+        y=_y;
+    }
+    Point()
+    {
+        x=0;
+        y=0;
+        cout<<"constructor point"<<endl;
+    }
+    ~Point()
+    {
+        cout<<"Destroy Point"<<endl;
+    }
+};
+class Shape
+{
+protected:
+    int color;
+    Point p1;
+    Point p2;
+public:
+    void setColor(int _color)
+    {
+        color=_color;
+    }
+    void setPoint1(int _x,int _y)
+    {
+        p1.set_xy(_x,_y);
+    }
+    void setPoint2(int _x,int _y)
+    {
+        p2.set_xy(_x,_y);
+    }
+    int getColor()
+    {
+        return color;
+    }
+    Point getPoint1()
+    {
+        return p1;
+    }
+    Point getPoint2()
+    {
+        return p2;
+    }
+    Shape()
+    {
+        color=1;
+        p1.set_xy(0,0);
+        p2.set_xy(0,0);
+        cout<<"Constructor shape"<<endl;
+    }
+    Shape(int p1x,int p1y,int p2x,int p2y,int _color):p1(p1x,p1y),p2(p2x,p2y)//take the value by parameter constructor of point instead of the setters for line
+    {
+        color=_color;
+    }
+    Shape(int p1x,int p1y,int _color):p1(p1x,p1y)//take the value by parameter constructor of point instead of the setters for circle
+    {
+        color=_color;
+    }
+    void Draw(){
+        cout<<"Draw in shape"<<endl;
+    }
 };
 
-class Circle : public {
-    Point p;
-    Point *ptr;
+class Line: public Shape{
+public:
+
+    Line(int p1x,int p1y,int p2x,int p2y,int _color) : Shape(p1x,p1y,p2x,p2y,_color)
+    {
+
+    }
+
+    Line():Shape()
+    {
+
+        //without using shape constructor
+        /*color=0;
+        p1.set_xy(0,0);
+        p2.set_xy(0,0);*/
+    }
+    void draw()
+    {
+        setColor(color);
+        line(p1.get_x(),p1.get_y(),p2.get_x(),p2.get_y());
+    }
+    friend istream & operator >> (istream &in,  Line &l);
+};
+
+//child class
+class Circle : public Shape
+{
     int radius;
 public:
-    Circle(){
-        p.set_xy(0,0);
+    Circle(int _radius,int px,int py, int _color):Shape( px, py, color)
+    {
+        radius=_radius;
+       /* p1.setPoint1(px,py);
+        color=_color;*/
+        //count<<"Circle Constructor"<<endl;
+    }
+    Circle():Shape()
+    {
         radius=0;
     }
-    Circle(int px,int py, int _radius){
-            p.set_xy(px,py);
-            radius=_radius;
-        }
-    void set_p(int _x,int _y){
-            p.set_xy(_x,_y);
-        }
-    void set_radius(int _radius){
+    void set_radius(int _radius)
+    {
         radius= _radius;
-        }
-    void set_circlePara(int x,int y ,int _radius){
-          p.set_xy(x,y);
-          radius= _radius;
-          circle(x,y,_radius);
-          getch();
-          closegraph();
-    }
-    void enterPoint(Point *p){
-     ptr=p;
-    }
-    void paintCircle2(){
-        ptr->paint();
-        cout<<radius<<endl;
-    }
-    void paintCircle(){
-        p.paint();
-        cout<<radius<<endl;
-    }
-    void paintCircle(Point x){//assoication
-        x.paint();
-        cout<<radius<<endl;
-    }
-    ~Circle(){
-         cout<<"Destroy circle"<<endl;
     }
 
-
+    void draw()
+    {
+        setcolor(color);
+        circle(p1.get_x(),p1.get_y(),radius);
+    }
+    friend istream & operator >> (istream &in,  Circle &c);
 };
+class Rect : public Shape
+{
+public:
+    Rect(int p1x, int p1y,int p2x,int p2y, int _color):Shape(p1x,p1y,p2x,p2y,_color)
+    {
+        /*
+        p1.set_xy(p1x,p1y);
+        p2.set_xy(p2x,p2y);
+        color=_color;
+        */
+    }
+    Rect():Shape(){
+
+    }
+    void draw()
+    {
+        setcolor(color);
+        rectangle(p1.get_x(),p1.get_y(),p2.get_x(),p2.get_y());
+    }
+    friend istream & operator >> (istream &in,  Rect &r);
+};
+
 
 
 int main()
@@ -110,9 +177,14 @@ int main()
     int number,type;
     cout << "Please enter the number of shapes you need" << endl;
     cin>> number;
-    for(int i=0;i<number;i++){
-    cout << "Please choose 0 to select circle, 1 to select Rectangle and 2 to select Line" << endl;
-    cin>> type;
+    int counter[10];
+    int arrRec[10];
+    int arrLine[10];
+    int arrCircle[10];
+    for(int i=0; i<number; i++)
+    {
+        cout << "Please choose 0 to select circle, 1 to select Rectangle and 2 to select Line" << endl;
+        cin>> arr[i];
     }
     return 0;
 }
